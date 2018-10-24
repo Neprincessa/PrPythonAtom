7faaf447cd969fac902c8a071fcfcdf886acf2c0
NastyaOmen <asya-iriska@yandex.ru>
2018-10-24 18:10:58 +0300

    KNN and flask started

--- homeworks/03/flask_json_server.py
+++ homeworks/03/flask_json_server.py
@@ -7,7 +7,9 @@ def return_classifier_result(version):
     #TODO прочитать из полученного запроса json-контент
     #В случае, если version==1, то должен вернуться json с версией и полем predict из входящего jsonа {"version":1, "predict":<predict_value>}
     #В случае, если version==0, то должен вернуться json с версией и полем old_predict из входящего jsonа {"version":0, "predict":<old_predict_value>}
-
+    content = request.get_json()
+     return json.dumps({"version": 1, "predict": r["predict"]})
+    return json.dumps({"version": 0, "predict": r["old_predict"]})
 @app.route("/")
 def hello():
     #TODO должна возвращатьс инструкция по работе с сервером
